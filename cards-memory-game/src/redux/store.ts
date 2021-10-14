15b3d1fd55efbdaecb3feb7b6a6de244b0454724
TaniaPashkevich <pashkevichtania2004@gmail.com>
2021-10-14 14:07:51 +0300

    feat: remove AppDispatch

--- cards-memory-game/src/redux/store.ts
+++ cards-memory-game/src/redux/store.ts
@@ -13,4 +13,4 @@ export default store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+//export type AppDispatch = typeof store.dispatch
