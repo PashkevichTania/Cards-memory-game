7daff3e4699a53a9e5227f0e27c15df05927ae30
TaniaPashkevich <pashkevichtania2004@gmail.com>
2021-10-14 14:46:22 +0300

    feat: add image and constants

---
+++ cards-memory-game/src/const.ts
@@ -0,0 +1,4 @@
+export const CARDS_IMAGES = [
+    'artangels.png', 'missantropocent.jpg', 'badlands.png', 'iichliwp.jpg',
+    'perezvoni.jpg', 'scaledandicy.jpg', 'thatsthespirit.jpg', 'nectar.png',
+]
