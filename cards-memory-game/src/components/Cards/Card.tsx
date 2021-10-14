639f4609b39a735c18ef33a593bbb7578707292a
TaniaPashkevich <pashkevichtania2004@gmail.com>
2021-10-14 15:32:21 +0300

    feat: add cards flip

--- cards-memory-game/src/components/Cards/Card.tsx
+++ cards-memory-game/src/components/Cards/Card.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Card = (props: {cardImage: string}) => {
 
-    const { cardImage } = props
+    const { cardImage } = props;
+
+    const [styleFront, setStyleFront] = useState({})
+    const [styleBack, setStyleBack] = useState({})
+
+    function flip() {
+        setStyleFront({transform: 'rotateY(180deg)'})
+        setStyleBack({transform: 'rotateY(360deg)'})
+    }
+    function flipBack() {
+        setStyleFront({})
+        setStyleBack({})
+    }
 
     return (
         <div className={"Card"}>
-            <img className={"cardImage"} src={`/cardsImages/${cardImage}`} alt={cardImage} />
+                <div className="front" onClick={flip} style={styleFront} id="cardFront">
+                    <img className={"cardImage"} src={`/cardsImages/thumbnail.jpg`} alt={"thumbnail"} />
+                </div>
+                <div className="back" onClick={flipBack} style={styleBack} id="cardBack">
+                    <img className={"cardImage"} src={`/cardsImages/${cardImage}`} alt={cardImage} />
+                </div>
         </div>
     );
 };
